36a30baebc13ba2b56dc61c38536752c668a889d
imhilla <hillaryodhiambo282@gmail.com>
2023-03-06 04:45:31 +0300

    add list

---
+++ datastructures/list.py
@@ -0,0 +1,13 @@
+# append list.append(x) add an item to the end of the list a[len(a):] = [x]
+# extend list.extend(iterable) extend the list by appending all items from the iterable
+# insert list.insert(i, x)
+# list.remove(x)
+# list.pop([i])
+# list.clear() eqv del a[:]
+# list.index()
+# list.count()
+# list.sort()
+# list.reverse()
+# list.copy()
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
