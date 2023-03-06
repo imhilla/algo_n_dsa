6682b6fd0ca806a1e445df44ee55c44b668094d2
imhilla <hillaryodhiambo282@gmail.com>
2023-03-06 04:59:38 +0300

    final

--- datastructures/list.py
+++ datastructures/list.py
@@ -11,3 +11,21 @@
 # list.copy()
 
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(fruits.count('apple'))
+# 2
+print(fruits.count('tangerine'))
+# 0
+print(fruits.index('banana'))
+# 3
+print(fruits.index('banana', 4))
+# 6 find next banana startinmg at position 4
+fruits.reverse()
+print(fruits)
+fruits.append('grape')
+print(fruits)
+fruits.sort()
+print(fruits)
+print(fruits.pop())
+print(fruits)
+
+# Using Lists as Stack
