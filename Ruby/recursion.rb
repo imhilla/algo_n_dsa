593816c582c0de75e7bbd22934523939bd5cbcca
imhilla <hillaryodhiambo282@gmail.com>
2023-07-15 10:39:31 +0300

    updates

--- Ruby/recursion.rb
+++ Ruby/recursion.rb
@@ -106,3 +106,43 @@ roman_mapping = {
   4 => "IV",
   1 => "I"
 }
+
+def integer_to_roman(roman_mapping, number, result="")
+  return result if number == 0
+  roman_mapping.keys.each do | divisor |
+    quotient, modulus = number.divmod(divisor)
+    result << roman_mapping[divisor] * quotient
+    return integer_to_roman(roman_mapping, modulus, result) if quotient > 0
+  end
+end
+
+puts integer_to_roman(roman_mapping, 38)
+
+roman_mapping = {
+  "M" => 1000,
+  "CM" => 900,
+  "D" => 500,
+  "CD" => 400,
+  "C" => 100,
+  "XC" => 90,
+  "L" => 50,
+  "XL" => 40,
+  "X" => 10,
+  "IX" => 9,
+  "V" => 5,
+  "IV" => 4,
+  "I" => 1
+}
+
+def roman_to_integer(roman_mapping, str, result = 0)
+  return result if str.empty?
+  roman_mapping.keys.each do |roman|
+    if str.start_with?(roman)
+      result += roman_mapping[roman]
+      str = str.slice(roman.length, str.length)
+      return roman_to_integer(roman_mapping, str, result)
+    end
+  end
+end
+
+puts roman_to_integer(roman_mapping, 'XXXVIII')
